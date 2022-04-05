63a154bfb7a12e6c67e3f3e76c366803d1522ce4
Blanche Jarrett <blanchejarrett@gmail.com>
2022-04-05 12:02:28 +0100

    functions challenges

--- 06-functions/main.js
+++ 06-functions/main.js
@@ -47,3 +47,40 @@ const calculatePerimeter = (width, height) => {
   }
   
   console.log(calculatePerimeter(1,1));
+
+// Write a function to find the area of a circle. when given a radius, print the area to the console.
+const calculateArea = (radius) => {
+    const area = (Math.PI * radius * radius);
+    return area
+    console.log(area)
+}
+console.log(calculateArea(2));
+
+// Now do the same to find the circumference of a circle
+
+const calculateCircumference = (radius) => {
+    const circumference = Math.PI * 2 * radius;
+    return circumference
+}
+console.log(calculateCircumference(1));
+
+// Write a function named calculateSupply that:
+// takes 2 arguments: age, amount per day.
+// calculates the amount consumed for rest of your life (based on a constant max age).
+// outputs the result to the screen like so: "You will need X to last you until the ripe old age of Y"
+// Call that function three times, passing in different values each time.
+// Bonus: Accept floating point values for amount per day, and round the result to a round number.
+
+const calculateSupply = (currentAge, amountPerDay) => {
+    const maxAge = 70;
+    const AVG_DAYS_YEARS = 365.25;
+    const remainingYears = maxAge - currentAge;
+    const totalSnickersPerLifetime = (amountPerDay * AVG_DAYS_YEARS * remainingYears);
+    // const message = ('You need ' + totalSnickersPerLifetime + ' supply of snickers to last you until the age of ' + maxAge);
+    console.log(`You need ${Math.round(totalSnickersPerLifetime)} supply of snickers to last you until the age of ${maxAge}`);
+  
+}
+calculateSupply(35, 5);
+calculateSupply(45, 2);
+calculateSupply(67, 11);
+ 
